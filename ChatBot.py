fbde6b4dff3d8e6c1c31a38a7ec899adb591ba68
Wanstey110 <46041920+Wanstey110@users.noreply.github.com>
2020-04-14 19:31:08 +0900

    Add files via upload

--- ChatBot.py
+++ ChatBot.py
@@ -24,20 +24,20 @@ getting data from entry of
 
 def info() :
     global myname
-    myname = entry_user.get('1.0', 'end-1c')
+    myname = entryUser.get('1.0', 'end-1c')
                                                                         
     global chatbot                           
-    chatbot = entry_chat.get('1.0', 'end-1c')
+    chatbot = entryChat.get('1.0', 'end-1c')
     
     if myname == "" or chatbot == "":
-        Label(frame_info , text = "Please fill in both fields" , bg = "red" , fg = "white" , font = 'Verdana 11 bold').place( x = 182 , y = 96)
+        Label(frameInfo , text = "Please fill in both fields" , bg = "red" , fg = "white" , font = 'Verdana 11 bold').place( x = 182 , y = 96)
         return 
 
-    entry_user.delete('1.0' , END)
-    entry_chat.delete('1.0' , END)
+    entryUser.delete('1.0' , END)
+    entryChat.delete('1.0' , END)
 
-    frame_info.pack_forget ()
-    frame_topic.pack ()
+    frameInfo.pack_forget ()
+    frameTopic.pack ()
     
 #------------------------------------------------------------------------------------------
 
@@ -47,9 +47,9 @@ opening files after selection of
                     page
 """
 
-def topic_1():
-    global no_topic
-    no_topic = 1
+def topic1():
+    global noTopic
+    noTopic = 1
     
     global top
     top = 'chat.txt'
@@ -60,13 +60,13 @@ def topic_1():
     global doc
     doc = a.readlines()
 
-    frame_topic.pack_forget()
-    frame_chat.pack()    
+    frameTopic.pack_forget()
+    frameChat.pack()    
 
     topic = "CoVID-19"
-    label_topic.config(text = topic)
+    labelTopic.config(text = topic)
 
-    refresh_screen()
+    refreshScreen()
 
 #----------------------------------------------------------------------------------------------------------
 
@@ -76,9 +76,9 @@ functions for writing in files
 
 """
 
-def write_ans () :
+def writeAns () :
 
-    enter1 = entry_feed.get('1.0' , END)
+    enter1 = entryFeed.get('1.0' , END)
 
     b.write(enter1)
     b.close()
@@ -90,22 +90,10 @@ def write_ans () :
            changes are saved
     """
 
-    if no_topic == 1 :
-        topic_1()
+    if noTopic == 1 :
+        topic1()
     
-    elif no_topic == 2 :
-        topic_2()          
-    
-    elif no_topic == 3 :
-        topic_3()
-    
-    elif no_topic == 4 :
-        topic_4()
-    
-    elif no_topic == 5 :
-        topic_5()
-    
-def feed_answer () :
+def feedAnswer () :
 
     """
 a seperate window for writing answer
@@ -116,18 +104,18 @@ a seperate window for writing answer
     global window
     window=Tk()
 
-    frame_root = Frame(window , bg = c1)
-    frame_root.pack()
+    frameRoot = Frame(window , bg = c1)
+    frameRoot.pack()
                                                                                             
-    label = Label (frame_root , text = 'Enter your answer to the Question here ...' , bg = c1 , fg = 'white' )
+    label = Label(frameRoot , text = 'Enter your answer to the Question here ...' , bg = c1 , fg = 'white' )
     label.pack()
 
-    global entry_feed
-    entry_feed = Text (frame_root , height = 3 , width = 30 , fg = 'white' , bg = c2)
-    entry_feed.bind ('<Return>' , write_ans)
-    entry_feed.pack()
+    global entryFeed
+    entryFeed = Text (frameRoot , height = 3 , width = 30 , fg = 'white' , bg = c2)
+    entryFeed.bind ('<Return>' , writeAns)
+    entryFeed.pack()
 
-    button = Button (frame_root , text = 'Add answer' , command  = write_ans , bg = c3 , fg = 'white' )
+    button = Button (frameRoot , text = 'Add answer' , command  = writeAns , bg = c3 , fg = 'white' )
     button.pack()
     
 def write_file () :
@@ -141,22 +129,22 @@ exsisting  files
     global b
     b = open ( top , 'a')
     
-    b.write(chat_raw)
+    b.write(chatRaw)
     b.write('\n')
 
-    button_write.place_forget()
-    feed_answer ()
+    buttonWrite.place_forget()
+    feedAnswer ()
 
 #----------------------------------------------------------------------------------------------
 
-def refresh_screen () :
+def refreshScreen () :
 
-    for widget in frame_chats.winfo_children():
+    for widget in frameChats.winfo_children():
         widget.destroy()
 
-    button_write.place_forget()
-    label_space = Label (frame_chats , bg = c1 ,  text = '')
-    label_space.pack()
+    buttonWrite.place_forget()
+    labelSpace = Label(frameChats , bg = c1 ,  text = '')
+    labelSpace.pack()
 
 #------------------------------------------------------------------------------------------
 def submit() :
@@ -167,19 +155,19 @@ function for producing response of
 
     """
 
-    button_write.place_forget ()
-    global chat_raw
-    chat_raw = entry.get('1.0', 'end-1c')
+    buttonWrite.place_forget ()
+    global chatRaw
+    chatRaw = entry.get('1.0', 'end-1c')
                         
     entry.delete('1.0' , END)
     
-    chat = chat_raw.lower()
+    chat = chatRaw.lower()
     chat = chat.replace(" ","")
 
-    global label_request
-    label_request = Label(frame_chats ,text=chat_raw , bg = c4 , fg= c7  , justify = LEFT , wraplength = 300, font = 'Verdana 10 bold')
+    global labelRequest
+    labelRequest = Label(frameChats ,text=chatRaw , bg = c4 , fg= c7  , justify = LEFT , wraplength = 300, font = 'Verdana 10 bold')
     
-    label_request.pack(anchor = 'w')   
+    labelRequest.pack(anchor = 'w')   
     
     global answer
     
@@ -230,7 +218,6 @@ function for producing response of
         placeHolder = cUpdates.cUpdate("Australia")
         answer = f"Current CoVID-19 statistics in Australia:\n{placeHolder}"
         
-
     elif chat == "sympcheck" or chat == "symptomcheck":
         simp = sympcheck.sympcheck()
         answer = simp
@@ -261,17 +248,17 @@ function for producing response of
                 answer == "I comprehend"
               elif chat.endswith("?"):
                 answer = "I'm sorry but I can't answer that"
-              button_write.place(x=430,y=3)
+              buttonWrite.place(x=430,y=3)
 
 
-    get_response()
+    getResponse()
         
-def get_response() :
+def getResponse() :
 
-    global label_response
-    label_response = Label(frame_chats ,text= answer ,bg= c5 , fg = c8 , justify = LEFT , wraplength = 300, font = 'Verdana 10 bold')
+    global labelResponse
+    labelResponse = Label(frameChats ,text= answer ,bg= c5 , fg = c8 , justify = LEFT , wraplength = 300, font = 'Verdana 10 bold')
 
-    label_response.pack(anchor = 'e')
+    labelResponse.pack(anchor = 'e')
 
     if answer ==  'Bye':
         root.destroy()
@@ -286,29 +273,29 @@ moving from one page to another
 
 """
 
-def welcome_to_info ():
-    frame_welcome.pack_forget ()
-    frame_info.pack ()
+def welcomeToInfo ():
+    frameWelcome.pack_forget ()
+    frameInfo.pack ()
     
-def info_to_topic ():
-    frame_info.pack_forget ()
-    frame_topic.pack ()
+def infoToTopic ():
+    frameInfo.pack_forget ()
+    frameTopic.pack ()
 
-def topic_to_chat ():
-    frame_topic.pack_forget ()
-    frame_chat.pack()
+def topicToChat ():
+    frameTopic.pack_forget ()
+    frameChat.pack()
 
-def chat_to_topic ():
-    frame_chat.pack_forget ()
-    frame_topic.pack ()
+def chatToTopic ():
+    frameChat.pack_forget ()
+    frameTopic.pack ()
 
-def topic_to_info ():
-    frame_topic.pack_forget ()
-    frame_info.pack ()
+def topicToInfo ():
+    frameTopic.pack_forget ()
+    frameInfo.pack ()
 
-def info_to_welcome ():
-    frame_info.pack_forget ()
-    frame_welcome.pack ()
+def infoToWelcome ():
+    frameInfo.pack_forget ()
+    frameWelcome.pack ()
 
 ##Initialize TKinter
 root = Tk ()  
@@ -326,21 +313,21 @@ submit_img = PhotoImage(file = 'image_8.png')
 """     WELCOME FRAME    """
 """    first frame containing time date and welcome messages """
 
-frame_welcome = Frame (root , bg = c1 , height = '670' , width = '550')
-frame_welcome.pack_propagate(0)
-frame_welcome.pack()                 
+frameWelcome = Frame(root , bg = c1 , height = '670' , width = '550')
+frameWelcome.pack_propagate(0)
+frameWelcome.pack()                 
 
   
-welcome = Label (frame_welcome , text = 'Welcome' , font = "Vardana 40 bold" , bg = c1 , fg = "white")
+welcome = Label(frameWelcome , text = 'Welcome' , font = "Vardana 40 bold" , bg = c1 , fg = "white")
 welcome.place(x = 160 , y = 200)
 
-welcome_chatbot = Label (frame_welcome , text = 'I am your CoVID-19 friendly chatbot!' , font = "Helvetica 15 bold italic" , bg = c1 , fg = c6)
-welcome_chatbot.place(x = 125 , y = 270)
+welcomeChatbot = Label(frameWelcome , text = 'I am your CoVID-19 friendly chatbot!' , font = "Helvetica 15 bold italic" , bg = c1 , fg = c6)
+welcomeChatbot.place(x = 125 , y = 270)
 
-pic_1 = Label (frame_welcome , image = screen_1)
-pic_1.place(x = -2 , y = 357 )
+pic1 = Label(frameWelcome , image = screen_1)
+pic1.place(x = -2 , y = 357 )
 
-button_front = Button (frame_welcome , image = front , relief = "flat" , bg = c1 , bd = "3px solid black" , command = welcome_to_info ).place(x=470 , y=10)
+buttonFront = Button (frameWelcome , image = front , relief = "flat" , bg = c1 , bd = "3px solid black" , command = welcomeToInfo ).place(x=470 , y=10)
 
 #__________________________________________________________________
 
@@ -348,12 +335,12 @@ button_front = Button (frame_welcome , image = front , relief = "flat" , bg = c1
 
 def clock () :
     current = time.strftime("%H:%M:%S")
-    label_time = Label (frame_welcome , bd = 5 ,  text = current , height = 1 , width = 8 , font = 'Ariel 11 bold' ,  fg = "white" , relief = 'groove' , bg = c3)
+    label_time = Label(frameWelcome , bd = 5 ,  text = current , height = 1 , width = 8 , font = 'Ariel 11 bold' ,  fg = "white" , relief = 'groove' , bg = c3)
     label_time.place(x= 120 , y = 63)
 
     label_time.after( 1000 , clock )
    
-button_time = Button (frame_welcome , text = 'Time' , height = 1 , font = 'Vardana 10 bold' ,  width = 8 , bg = c2 , fg = c1 ,  command = clock)
+button_time = Button (frameWelcome , text = 'Time' , height = 1 , font = 'Vardana 10 bold' ,  width = 8 , bg = c2 , fg = c1 ,  command = clock)
 button_time.place(x=30 , y = 63)
 
 #_____________________________________________________________________________
@@ -364,7 +351,7 @@ def date () :
     
     try:
         date = time.strftime("%d %B , 20%y")
-        label_date = Label (frame_welcome , bd = 5 , relief = 'groove' ,  text = date , bg = c3 , fg = "white"  , height = 1 , font = 'Ariel 11 bold')
+        label_date = Label(frameWelcome , bd = 5 , relief = 'groove' ,  text = date , bg = c3 , fg = "white"  , height = 1 , font = 'Ariel 11 bold')
         label_date.place(x= 400 , y = 63)
 
         label_date.after(86400000 , date)
@@ -373,7 +360,7 @@ def date () :
         print('')        
         
         
-button_date = Button (frame_welcome , text = 'Date' ,height = 1 , font = 'Vardana 10 bold' ,  width = 8 , bg = c2 , fg= c1 , command = date)
+button_date = Button (frameWelcome , text = 'Date' ,height = 1 , font = 'Vardana 10 bold' ,  width = 8 , bg = c2 , fg= c1 , command = date)
 button_date.place(x = 310 , y = 63)
 
 #+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -381,54 +368,54 @@ button_date.place(x = 310 , y = 63)
 """     INFO FRAME   """
 """     frame of entering names    """
 
-frame_info = Frame (root , bg = c1 , height = '670' , width = '550')
-frame_info.pack_propagate(0)
+frameInfo = Frame(root , bg = c1 , height = '670' , width = '550')
+frameInfo.pack_propagate(0)
 
-spacer1 = Label(frame_info , bg = c1)
+spacer1 = Label(frameInfo , bg = c1)
 spacer1.pack()
 
-spacer2 = Label(frame_info , bg = c1)
+spacer2 = Label(frameInfo , bg = c1)
 spacer2.pack()
 
-label_sub = Label (frame_info ,text = "Enter name information" , bg = c1 , fg = "white" , font = 'Verdana 30 italic')
-label_sub.pack()
+labelSub = Label(frameInfo ,text = "Enter name information" , bg = c1 , fg = "white" , font = 'Verdana 30 italic')
+labelSub.pack()
                             
-user_name = Label (frame_info , text = 'What should the chatbot call you? ' , bg = c1 , fg = c2 , font = 'Ariel 15')
-user_name.place(x = 80,y=130)
+username = Label(frameInfo , text = 'What should the chatbot call you? ' , bg = c1 , fg = c2 , font = 'Ariel 15')
+username.place(x = 80,y=130)
 
-entry_user = Text (frame_info , bg = c6, fg = "white" , height ='1'  , width ='40' , font = 'Ariel 15')
-entry_user.focus()
-entry_user.place(x = 80 , y = 170)
+entryUser = Text (frameInfo , bg = c6, fg = "white" , height ='1'  , width ='40' , font = 'Ariel 15')
+entryUser.focus()
+entryUser.place(x = 80 , y = 170)
 
-chatbot_name = Label (frame_info , text = 'What do you want to call the chatbot? ' , bg = c1 , fg = c2 , font = 'Ariel 15')
-chatbot_name.place(x = 80 , y = 220)
+chatbotName = Label(frameInfo , text = 'What do you want to call the chatbot? ' , bg = c1 , fg = c2 , font = 'Ariel 15')
+chatbotName.place(x = 80 , y = 220)
 
-entry_chat = Text (frame_info , bg = c6, fg = "white" , height ='1'  , width ='40' , font = 'Ariel 15')
-entry_chat.place(x = 80 , y = 260)
+entryChat = Text (frameInfo , bg = c6, fg = "white" , height ='1'  , width ='40' , font = 'Ariel 15')
+entryChat.place(x = 80 , y = 260)
 
-button_1 = Button (frame_info , text ='submit' , font = 'Vardana 10 bold' , bg = c2 , fg = c1 , command = info )
+button_1 = Button (frameInfo , text ='submit' , font = 'Vardana 10 bold' , bg = c2 , fg = c1 , command = info )
 button_1.place(x = 470 , y = 330)
 
-button_back = Button (frame_info , image =  back , relief = "flat" , bg = c1 , command = info_to_welcome).place(x=10 , y = 10)
+buttonBack = Button (frameInfo , image =  back , relief = "flat" , bg = c1 , command = infoToWelcome).place(x=10 , y = 10)
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 """     TOPIC FRAME   """
 """"   frame for topic selection     """
 
-frame_topic = Frame (root , bg = c1 , height = '670' , width = '550')
-frame_topic.pack_propagate(0)
+frameTopic = Frame(root , bg = c1 , height = '670' , width = '550')
+frameTopic.pack_propagate(0)
                             
-spacer3 = Label(frame_topic , bg = c1)
+spacer3 = Label(frameTopic , bg = c1)
 spacer3.pack()
 
-spacer4 = Label(frame_topic , bg = c1)
+spacer4 = Label(frameTopic , bg = c1)
 spacer4.pack()
 
-spacer5 = Label(frame_topic , bg = c1)
+spacer5 = Label(frameTopic , bg = c1)
 spacer5.pack()
 
-option_1 = Label (frame_topic , text = """
+option_1 = Label(frameTopic , text = """
     Make sure you type in lowercase without spaces! 
     E.g. whatisyourname?
     If you're stuck, type help
@@ -436,50 +423,50 @@ option_1 = Label (frame_topic , text = """
     """ , font = 'Verdana 15 italic' , bg = c1 , fg= c2)
 option_1.place(x = 0 , y = 50)
 
-button_opt_1 = Button (frame_topic , text = 'Proceed' , image = front , relief = "flat" , bg = c1 ,command = topic_1)
+button_opt_1 = Button (frameTopic , text = 'Proceed' , image = front , relief = "flat" , bg = c1 ,command = topic1)
 button_opt_1.place(x = 350 , y = 50)
 
-button_back = Button (frame_topic , image = back , relief = "flat", bg = c1 , command = topic_to_info).place(x=10 , y = 10)
+buttonBack = Button (frameTopic , image = back , relief = "flat", bg = c1 , command = topicToInfo).place(x=10 , y = 10)
 
 """         CHAT FRAME   """
 """"       Main Chat Screen   """
 
-frame_chat = Frame (root , bg = c1 , height = '670' , width = '550')
-frame_chat.pack_propagate(0)
+frameChat = Frame(root , bg = c1 , height = '670' , width = '550')
+frameChat.pack_propagate(0)
 
-frame_top = Frame( frame_chat , bg = c3 , height = '100' , width = '550')
+frame_top = Frame( frameChat , bg = c3 , height = '100' , width = '550')
 frame_top.pack()
 
-label_topic = Label ( frame_top , bg = c3 , fg = 'white' , font = 'Verdana 20 bold ')
-label_topic.pack(pady = '40')
+labelTopic = Label( frame_top , bg = c3 , fg = 'white' , font = 'Verdana 20 bold ')
+labelTopic.pack(pady = '40')
 
 frame_spacer = Frame( frame_top , bg = c2 , height = "10" , width = "550" )
 frame_spacer.pack()
 
-bottom_frame = Frame (frame_chat , bg = c2 , height = '100' , width = '550')
-bottom_frame.pack_propagate(0)
-bottom_frame.pack(side = BOTTOM)
+bottomFrame = Frame(frameChat , bg = c2 , height = '100' , width = '550')
+bottomFrame.pack_propagate(0)
+bottomFrame.pack(side = BOTTOM)
 
-button = Button (bottom_frame , image = submit_img , relief = "flat", font = 'Vardana 10 bold' , bg = c3 , command = submit )
+button = Button (bottomFrame , image = submit_img , relief = "flat", font = 'Vardana 10 bold' , bg = c3 , command = submit )
 button.place(x = 410 , y = 27)
                                    
-entry = Text (bottom_frame , bg = c3 , fg = c6 , height = '5'  , width ='45' , font  ='Verdana 10')
+entry = Text (bottomFrame , bg = c3 , fg = c6 , height = '5'  , width ='45' , font  ='Verdana 10')
 entry.bind ('<Return>' , submit)
 entry.place(x = 30, y = 10)
 
-frame_chats = Frame (frame_chat , bg = c1 , height = '450' , width = '500' )
-frame_chats.pack_propagate (0)
-frame_chats.pack()
+frameChats = Frame(frameChat , bg = c1 , height = '450' , width = '500' )
+frameChats.pack_propagate (0)
+frameChats.pack()
 
-label_space = Label(frame_chats , bg = c1).pack()
+labelSpace = Label(frameChats , bg = c1).pack()
 
-button_refresh = Button (frame_chat , bg = c3 , fg = c2 ,  text = 'refresh' , font = 'Vardana 10 bold' ,  command =refresh_screen)
-button_refresh.place(x = 440 , y = 80)
+buttonRefresh = Button (frameChat , bg = c3 , fg = c2 ,  text = 'refresh' , font = 'Vardana 10 bold' ,  command =refreshScreen)
+buttonRefresh.place(x = 440 , y = 80)
 
-button_write = Button (bottom_frame , text = 'write' ,bg = c3 ,fg = c2 , font = 'Vardana 8' ,  command = write_file )
+buttonWrite = Button (bottomFrame , text = 'write' ,bg = c3 ,fg = c2 , font = 'Vardana 8' ,  command = write_file )
 
-button_back = Button (frame_chat , image = back , relief = "flat" , bg = c3 , command = chat_to_topic).place(x=10 , y = 10)
-button_front = Button (frame_chat , image = exitt , relief = "flat" , bg = c3 , command = root.destroy ).place(x=440 , y = 10)
+buttonBack = Button (frameChat , image = back , relief = "flat" , bg = c3 , command = chatToTopic).place(x=10 , y = 10)
+buttonFront = Button (frameChat , image = exitt , relief = "flat" , bg = c3 , command = root.destroy ).place(x=440 , y = 10)
 
 root.mainloop ()
 ##Well done you made it!
--- cUpdates.py
+++ cUpdates.py
@@ -8,7 +8,7 @@ def cUpdate(country):
 	    'x-rapidapi-key': "b087d3c483msha78ec3f9ef8104cp1b83e5jsn6eb7ac519d97"
 	    }
 	response = requests.request("GET", url, headers=headers, params=querystring)
-	result = response.text.find("confirmed")
+	result = response.text.find('"confirmed')
 	toPrint = response.text[result:].replace("}", "")
 	toPrint = toPrint.replace("]", "")
 	return toPrint
