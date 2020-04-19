9f355bc8f17f9fe9174e8e17a3b97311e63a0ca2
HotPogger <will.anstey2005@gmail.com>
2020-04-19 19:02:04 +0900

    Update Things
    Co-Authored-By: dce521 <dce521@users.noreply.github.com>

--- cUpdateWorld.py
+++ cUpdateWorld.py
@@ -1,23 +1,32 @@
 import requests
 def main():
-    url = "https://covid-19-statistics.p.rapidapi.com/provinces"
-
-    querystring = {"iso":"CHN"}
-
+    log = open("log.txt", "w")
+    url = "https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats"
+    querystring = {"country": "USA"}
     headers = {
-        'x-rapidapi-host': "covid-19-statistics.p.rapidapi.com",
-        'x-rapidapi-key': "96c395c12bmsh7977f2a7f725335p156aa1jsn3256842e9bbc"
+	    'x-rapidapi-host': "covid-19-coronavirus-statistics.p.rapidapi.com",
+	    'x-rapidapi-key': "b087d3c483msha78ec3f9ef8104cp1b83e5jsn6eb7ac519d97"
         }
-
     response = requests.request("GET", url, headers=headers, params=querystring)
 
     r = response.text
     r = r.replace('"','')
     r = r.replace('{','')
     r = r.replace('}','')
-    r = r.replace(',','')
+    r = r.replace(',',' ')
+    r = r.replace('covid19Stats:[',"")
 
     ##Get info
+    tConf = []
+    while r.find("confirmed:") != 0:
+        conf = r.find("confirmed")
+        confEnd = r.find(" deaths:")
+        x = r[conf+10:confEnd]
+        log.write(x)
+        ##print(x)
+        tConf.append(int(x))
+        ##r = r[conf+10:confEnd:]
+    print(sum(tConf))
     return r
 if __name__ == "__main__":
     x = main()
