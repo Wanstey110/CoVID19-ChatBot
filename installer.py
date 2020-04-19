702a1da59dd5797c67f9d757c4fdd7ef98197236
HotPogger <46041920+Wanstey110@users.noreply.github.com>
2020-04-19 10:05:07 +0900

    Add files via upload

--- installer.py
+++ installer.py
@@ -1,6 +1,6 @@
 import requests
 filenames = ["Chat.txt", "ChatBot.py", "cUpdates.py",
-             "growthRate.py", "Hacker.py", "sympcheck.py"]
+             "growthRate.py", "sympcheck.py"]
 for i in filenames:
     response = requests.get(
         f"https://raw.github.com/Wanstey110/CoVID19-ChatBot/Wanstey110-patch-1/{i}")
